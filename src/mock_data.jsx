331c52bc6557712191b31eb7a3e66adcd6636b94
manellylopez <manellylopezqqq@gmail.com>
2025-04-02 10:55:14 -0500

    update url of the images

--- src/mock_data.jsx
+++ src/mock_data.jsx
@@ -3,10 +3,10 @@ const mock_data_1 = {
   startedAt: new Date(),
   picture: {
     src_image:
-      "https://res.cloudinary.com/dwlqplcgt/image/upload/v1742854639/Waldo_galactic_city_lzdru1.png",
+      "https://res.cloudinary.com/dwlqplcgt/image/upload/v1743608952/find_the_charaters_in_the_galactic_city_pcuwu3.png",
     characters: [
       { id: "char-1", name: "Wally", found: false, x: 0, y: 0 },
-      { id: "char-2", name: "The Wizard", found: false, x: 0, y: 0 },
+      { id: "char-2", name: "R2D2", found: false, x: 0, y: 0 },
       {
         id: "char-3",
         name: "Courage the Cowardly Dog",
@@ -22,7 +22,7 @@ const mock_data_2 = {
   game_id: "generate_id",
   startedAt: new Date(),
   picture: {
-    src_image: "https://",
+    src_image: "https://res.cloudinary.com/dwlqplcgt/image/upload/v1743608911/find_the_lucky_clover_mqkdmu.png",
     characters: [
       { id: "char-4", name: "Four Leaf Clover", found: false, x: 0, y: 0 },
     ],
