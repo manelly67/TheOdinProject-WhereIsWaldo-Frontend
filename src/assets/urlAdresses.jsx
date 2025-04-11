4ccbd53e34d190e23bde11af724869f6523b3057
manellylopez <manellylopezqqq@gmail.com>
2025-04-11 14:52:13 -0500

    create file url adresses

---
+++ src/assets/urlAdresses.jsx
@@ -0,0 +1,10 @@
+const base = "https://top-backend-gamewaldo.onrender.com";
+
+const urlAddresses = {
+    "home" : `${base}/`,
+    "session" : `${base}/session-id`,
+    "getplayer" : `${base}/players/player_for_session`,
+};
+
+
+export { urlAddresses };
