eb8d7ac9fe3476b748acb6642b00ce9be3e1e4d8
manellylopez <manellylopezqqq@gmail.com>
2025-04-15 18:57:38 -0500

    modify url base address

--- src/assets/urlAdresses.jsx
+++ src/assets/urlAdresses.jsx
@@ -1,11 +1,12 @@
-const base = "https://top-backend-gamewaldo.onrender.com";
+const base = "https://top-whereis-backend.onrender.com";
 
 const urlAddresses = {
-    "home" : `${base}/`,
-    "session" : `${base}/session-id`,
-    "getplayer" : `${base}/players/player_for_session`,
-    "createGame" : `${base}/games/new`,
+  home: `${base}/`,
+  session: `${base}/session-id`,
+  createplayer: `${base}/players/player_for_session`,
+  readplayer: `${base}/players/id`,
+  createGame: `${base}/games/new`,
+  round: `${base}/games/round`,
 };
-
 
 export { urlAddresses };
